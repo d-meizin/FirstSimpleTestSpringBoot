e21e375cc20118c2aed42dda82110ca33bc8ec56
d-meizin <d.meizin@gmail.com>
2025-07-23 16:42:34 +0300

    initialize commit

---
+++ src/main/java/meizin/ltd/FirstSimpleTestSpringBoot/FirstSimpleTestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package meizin.ltd.FirstSimpleTestSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSimpleTestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSimpleTestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/meizin/ltd/FirstSimpleTestSpringBoot/FirstSimpleTestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package meizin.ltd.FirstSimpleTestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSimpleTestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
