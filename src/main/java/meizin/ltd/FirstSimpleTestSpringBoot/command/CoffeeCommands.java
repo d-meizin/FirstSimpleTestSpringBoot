63181e866a159314057c49b8be90e63e671f7836
d-meizin <d.meizin@gmail.com>
2025-08-11 14:33:59 +0300

    changed class CoffeeCommands

--- src/main/java/meizin/ltd/FirstSimpleTestSpringBoot/command/CoffeeCommands.java
+++ src/main/java/meizin/ltd/FirstSimpleTestSpringBoot/command/CoffeeCommands.java
@@ -2,6 +2,8 @@ package meizin.ltd.FirstSimpleTestSpringBoot.command;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
 import meizin.ltd.FirstSimpleTestSpringBoot.bean.CoffeeShop;
 import meizin.ltd.FirstSimpleTestSpringBoot.model.CoffeeType;
 import meizin.ltd.FirstSimpleTestSpringBoot.model.CoffeeOrder;
@@ -16,8 +18,8 @@ public class CoffeeCommands {
 		
 	}
 	
-	@ShellMethod("Заказать кофе")
-	public String orderCoffee(String coffeeType) {
+	@ShellMethod(value = "Заказать кофе", key = "zakaz")
+	public String orderCoffee(@ShellOption(value = "t") String coffeeType) {
 		try {
 			CoffeeOrder order = new CoffeeOrder(CoffeeType.valueOf(coffeeType.toUpperCase()));
 			return coffeeShop.processOrder(order);
